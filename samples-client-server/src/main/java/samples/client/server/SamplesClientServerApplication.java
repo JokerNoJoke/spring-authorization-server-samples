1b646e04ed8ca9b9e1159b42d458f48d36637d7b
joker <joekr9166@outlook.com>
2023-11-06 22:17:01 +0800

    add modules

---
+++ samples-client-server/src/main/java/samples/client/server/SamplesClientServerApplication.java
@@ -0,0 +1,13 @@
+package samples.client.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplesClientServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplesClientServerApplication.class, args);
+    }
+
+}
---
+++ samples-resource-server/src/main/java/samples/resource/server/SamplesResourceServerApplication.java
@@ -0,0 +1,13 @@
+package samples.resource.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplesResourceServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplesResourceServerApplication.class, args);
+    }
+
+}
